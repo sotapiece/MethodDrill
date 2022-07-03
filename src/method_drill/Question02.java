0b678ca39a48ae15699ab8bb07c7a987e7989639
yamazaki sota <kizumini10@gmail.com>
2022-07-03 21:20:37 +0900

    Question2

---
+++ src/method_drill/Question02.java
@@ -0,0 +1,12 @@
+package method_drill;
+
+public class Question02 {
+	
+	static void printPI() {
+		System.out.println(Math.PI);
+	}
+	
+	public static void main(String[] args) {
+		printPI();
+	}
+}
