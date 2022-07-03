0891ab64743bf63e1e29f1fc1543a00cbcd51468
yamazaki sota <kizumini10@gmail.com>
2022-07-03 21:39:08 +0900

    Question3

---
+++ src/method_drill/Question04.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question04 {
+	static void printMessage(String message) {
+		System.out.println(message);
+	}
+	
+	public static void main(String[] args) {
+		printMessage("Hello");
+	}
+}
