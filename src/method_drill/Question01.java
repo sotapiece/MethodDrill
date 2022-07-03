f023c4b256deae70faecb54165b462f1c1e5a5e6
yamazaki sota <kizumini10@gmail.com>
2022-07-03 21:15:15 +0900

    Question1

--- src/method_drill/Question01.java
+++ src/method_drill/Question01.java
@@ -1,5 +1,12 @@
 package method_drill;
 
 public class Question01 {
-
+	
+	static void printHello() {
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		printHello();
+	}
 }
