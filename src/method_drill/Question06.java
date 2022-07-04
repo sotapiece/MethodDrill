6c365ad291e0f6eb31791e64847147438490b082
yamazaki sota <kizumini10@gmail.com>
2022-07-04 16:15:08 +0900

    Question6

---
+++ src/method_drill/Question06.java
@@ -0,0 +1,18 @@
+package method_drill;
+
+public class Question06 {
+	static void printRandomMessage(String name) {
+		int n = (int)(3*Math.random());
+		if(n == 0) {
+			System.out.println("こんばんは"+name+"さん");
+		}else if(n == 1) {
+			System.out.println("こんにちは"+name+"さん");
+		}else if(n == 2) {
+			System.out.println("おはよう"+name+"さん");
+		}
+	}
+	
+	public static void main(String[] args) {
+		printRandomMessage("こころ");
+	}
+}
