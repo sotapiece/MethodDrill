abba9555d3e20ab5bbefd2d049edd69dd4731681
yamazaki sota <kizumini10@gmail.com>
2022-07-04 17:23:29 +0900

    Question10

---
+++ src/method_drill/Question10.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question10 {
+	static String getMethod() {
+		return "よろしくお願いいたします";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getMethod());
+	}
+}
