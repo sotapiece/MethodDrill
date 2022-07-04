dfc2b15fd21be4eb1032e3448c278aea89a03c83
yamazaki sota <kizumini10@gmail.com>
2022-07-04 17:44:32 +0900

    Question12

---
+++ src/method_drill/Question12.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question12 {
+	static double getSquareRootOf2() {
+		return Math.sqrt(2.0);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getSquareRootOf2());
+	}
+}
