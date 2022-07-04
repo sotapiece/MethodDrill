ebf25bbcd31eb442a20784aa17aa9ff4b5adfe25
yamazaki sota <kizumini10@gmail.com>
2022-07-04 17:08:17 +0900

    Question8

---
+++ src/method_drill/Question08.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question08 {
+	static void printRectangleArea(double height, double width) {
+		System.out.println("高さが"+height+", 重さが"+width+"の長方形の面積は"+height*width);
+	}
+	
+	public static void main(String[] args) {
+		printRectangleArea(4.5,8.7);
+	}
+}
