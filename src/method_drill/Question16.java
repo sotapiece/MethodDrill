46b8900fbc6a7f7f2f4dcd37e36229b9c4871f1d
yamazaki sota <kizumini10@gmail.com>
2022-07-04 21:19:25 +0900

    Question16

---
+++ src/method_drill/Question16.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question16 {
+	static double getMinValue(double a, double b) {
+		return Math.min(a, b);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getMinValue(2.5,8.6));
+	}
+}
