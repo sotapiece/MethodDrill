acb725e1ceed6d7a3cbecced14248ae1692ca845
yamazaki sota <kizumini10@gmail.com>
2022-07-04 21:11:12 +0900

    Question15

---
+++ src/method_drill/Question15.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question15 {
+	static boolean isEvenNumber(int value) {
+		return value%2 == 0;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isEvenNumber(7));
+	}
+}
