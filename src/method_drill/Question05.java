0b4af9f6daa52827fe136783d246c2eb60caeea6
yamazaki sota <kizumini10@gmail.com>
2022-07-04 16:04:33 +0900

    Question5

---
+++ src/method_drill/Question05.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question05 {
+	static void printCircleArea(double radius) {
+		System.out.println(radius * radius * Math.PI);
+	}
+	
+	public static void main(String[] args) {
+		printCircleArea(2.0);
+	}
+}
