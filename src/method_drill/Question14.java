ea2262889b1ce52acc416c66a806de00ce8dfc9e
yamazaki sota <kizumini10@gmail.com>
2022-07-04 21:07:26 +0900

    Question14

---
+++ src/method_drill/Question14.java
@@ -0,0 +1,11 @@
+package method_drill;
+
+public class Question14 {
+	static double getAbsoluteValue(double value) {
+		return Math.abs(value);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getAbsoluteValue(-4.9));
+	}
+}
